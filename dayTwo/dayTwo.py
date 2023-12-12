6babaf0b39939c85b819dbabf4d1060e98a6fc5b
UsernameTaken420 <Ngabin1999@gmail.com>
2023-12-12 20:02:24 -0300

    solved day two part two

--- dayTwo/dayTwo.py
+++ dayTwo/dayTwo.py
@@ -2,7 +2,6 @@ import string
 
 # Part One
 data = open("dayTwoInput.txt", "r")
-colors = ["green", "red", "blue"]
 maxRed = 12
 maxGreen = 13
 maxBlue = 14 
@@ -32,3 +31,24 @@ print(f"Part one sum of IDs is: {idSum}")
 
 # Part Two
 data = open("dayTwoInput.txt", "r")
+totalSum = 0
+for line in data:
+    minRed = -1
+    minBlue = -1
+    minGreen = -1
+    gameData = line.split(':')[1].split(";")
+    for round in gameData:
+        colors = round.split(',')
+        for color in colors:
+            match color.split(" ")[2].split("\n")[0]:
+                case "green":
+                    if int(color.split(" ")[1]) > minGreen:
+                        minGreen = int(color.split(" ")[1])
+                case "blue":
+                    if int(color.split(" ")[1]) > minBlue:
+                        minBlue = int(color.split(" ")[1])
+                case "red":
+                    if int(color.split(" ")[1]) > minRed:
+                        minRed = int(color.split(" ")[1])
+    totalSum+= (minRed * minGreen * minBlue)
+print(f"Part two sum of powers is: {totalSum}")
