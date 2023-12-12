06dbb69edf9477261b1a245665799df9eb277224
UsernameTaken420 <Ngabin1999@gmail.com>
2023-12-12 19:04:36 -0300

    day one solved with regex

--- dayOne/dayOne.py
+++ dayOne/dayOne.py
@@ -1,10 +1,8 @@
-import os
-import string
+import re
 
-
-data = open("dayOneInput.txt", "r")
 # Part One
-totalsum = 0
+data = open("dayOneInput.txt", "r")
+partOneSum = 0
 for line in data:
     firstNum = 0
     lastNum = 0
@@ -13,6 +11,27 @@ for line in data:
             if firstNum==0:
                 firstNum = char
             lastNum = char
-    numberGen = firstNum + lastNum
-    totalsum+=int(numberGen)
-print(totalsum)
+    numberGen = str(firstNum) + str(lastNum)
+    partOneSum+=int(numberGen)
+print(f"The final sum for part one is: {partOneSum}")
+
+# Part Two
+data = open("dayOneInput.txt", "r")
+textNumbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+partTwoSum = 0
+for line in data:
+    tempLine = line
+    digits = re.findall(r'(?=(one|two|three|four|five|six|seven|eight|nine|\d))', tempLine)
+    for number in digits:
+        if number in textNumbers:
+            digits[digits.index(number)] = str(textNumbers[number])
+    firstNum = 0
+    lastNum = 0
+    for char in digits:
+        if char.isnumeric():
+            if firstNum == 0:
+                firstNum = char
+            lastNum = char
+    numberGen = str(firstNum) + str(lastNum)
+    partTwoSum+=int(numberGen)
+print(f"The final sum port part two is: {partTwoSum}")
